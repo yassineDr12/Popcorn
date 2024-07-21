ac56eeaa7fe4642556bd71af1969c9e486459c35
Yassine Drafate <yassinedrafate1@gmail.com>
2024-07-21 12:06:43 +0100

    secure API key

--- src/hooks/useMovies.tsx
+++ src/hooks/useMovies.tsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react";
 import { IMovie } from "../dataTypes";
 import axios from "axios";
 
+const API_KEY = process.env.REACT_APP_OMDB_API_KEY;
+
 const useMovies = () => {
   const [searchQuery, setSearchQuery] = useState<string | undefined>(undefined);
   const [searchResults, setSearchResults] = useState<IMovie[]>([]);
@@ -12,13 +14,13 @@ const useMovies = () => {
       if (searchQuery) {
         setMovieSearchLoading(true);
         try {
-          const response = await axios.get(`https://www.omdbapi.com/?s=${searchQuery}&apikey=dbc2c0f9`);
+          const response = await axios.get(`https://www.omdbapi.com/?s=${searchQuery}&apikey=${API_KEY}`);
           const movieList = response.data.Search;
 
           if (movieList) {
             const detailedMovieList: IMovie[] = await Promise.all(
               movieList.map(async (movie: IMovie) => {
-                const response = await axios.get(`https://www.omdbapi.com/?i=${movie.imdbID}&apikey=dbc2c0f9`);
+                const response = await axios.get(`https://www.omdbapi.com/?i=${movie.imdbID}&apikey=${API_KEY}`);
                 const movieData = response.data;
 
                 return {
@@ -29,8 +31,8 @@ const useMovies = () => {
                   Poster: movieData.Poster,
                   Genre: movieData.Genre,
                   Plot: movieData.Plot,
-                  Length: movieData.Runtime, // Assuming Runtime represents movie length
-                  imdbRating: parseFloat(movieData.imdbRating), // Convert imdbRating to number
+                  Length: movieData.Runtime,
+                  imdbRating: parseFloat(movieData.imdbRating),
                 };
               })
             );
